359240ab2d10371154f91bf5140b60c3e3c5a567
Moustapha <amroune.moustapha@gmail.com>
2025-05-04 13:42:02 +0200

    add onKeyDown and button description

--- src/App.tsx
+++ src/App.tsx
@@ -52,9 +52,18 @@ const audioClips: AudioClip[] = [
 
 function App() {
 
+  const playAudio = (e: React.KeyboardEvent<HTMLDivElement>) =>{
+    const clip = audioClips.find((clip)=> clip.keyTrigger === e.key.toUpperCase());
+    if(!clip) return;
+    (document.getElementById(clip.keyTrigger) as HTMLAudioElement).play().catch(console.error);
+
+    
+    document.getElementById("drum-" + clip.keyTrigger)?.focus();
+    document.getElementById("display")!.innerText = clip.description;
+  }
 
   return (
-    <div className="container" id="drum-machine">
+    <div className="container" id="drum-machine" onKeyDown={playAudio}>
       <h1>FCC Drum Machine</h1>
       <div className="whole-drum">
         {audioClips.map((clip) => (
